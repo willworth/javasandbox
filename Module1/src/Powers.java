6a66d5ebde9bf83c95154e03bf628ebac96a4c9a
Will Worth <will@willworth.co.uk>
2016-06-22 15:01:55 +0200

    Simple text layout exercise and improved powers loop.

--- Module1/src/Powers.java
+++ Module1/src/Powers.java
@@ -6,10 +6,10 @@
  */
 public class Powers {
     public static void main (String[] args){
-        for(int i = 1; i<21; i++ ){
-            System.out.println("When i is: " + i);
-            System.out.println("The square of " +i+ " is "+ (i*i));
-            System.out.println("The cube of " +i+ " is " + (i*i*i));
+        for(int i = 1; i<51; i++ ){
+            System.out.print("When i is " + i);
+            System.out.print(", the square of " +i+ " is "+ (i*i));
+            System.out.println(", and the cube of " +i+ " is " + (i*i*i));
         }
 
     }
