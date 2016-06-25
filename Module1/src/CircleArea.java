755f3920d9ff33e3199f6c845273f2615fd8a6fd
Will Worth <will@willworth.co.uk>
2016-06-25 16:58:31 +0200

    maths api testing

--- Module1/src/CircleArea.java
+++ Module1/src/CircleArea.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class CircleArea {
     public static void main (String [] args){
+        final double PI = 3.14159;
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a number for radius: ");
-
+        //works the same for byte short int long float
         double radius = input.nextDouble();
-        double area = radius * radius * 3.14159;
+        double area = radius * radius * PI;  //constants all upper case
 
         System.out.println("The area for the circle of radius " + radius + " is " + area);
 
---
+++ Module1/src/Maths.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Noelia on 25-Jun-16.
+ */
+public class Maths {
+
+    public static void main (String[] args){
+
+        System.out.print(Math.pow(2, 3));
+
+
+    }
+}
