88c1928cad977f8616a5f818eb108f845a02c52e
Will Worth <will@willworth.co.uk>
2016-06-26 20:07:35 +0200

    maths api testing

--- Module1/src/Maths.java
+++ Module1/src/Maths.java
@@ -5,7 +5,16 @@ public class Maths {
 
     public static void main (String[] args){
 
-        System.out.print(Math.pow(2, 3));
+        System.out.println(Math.pow(2, 3));
+        System.out.println(2*(5/2 +5/2));// =2*2+2
+        System.out.println(2*5/2+2*5/2); //=10/2+ 10/2
+        System.out.println(1.254E3);// exponents
+        System.out.println(1.254E-3);
+        long creditCardNumber = 1535_46568_4665L;
+        System.out.println("Here is a difficult to read long number:"
+                + creditCardNumber);//underscores for readability
+
+
 
 
     }
