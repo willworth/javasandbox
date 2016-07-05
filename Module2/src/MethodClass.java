664149ce6e36b5161f2a451d5091ac958371474d
Will Worth <will@willworth.co.uk>
2016-07-05 18:31:19 +0200

    methods play

---
+++ Module2/src/MethodClass.java
@@ -0,0 +1,28 @@
+
+public class MethodClass {
+
+
+    //simple method
+
+    //return type    method name   (values)
+    public static int mySimpleSummingMethod (int a, int b){
+            int ans = a + b;
+            //return type must be the same as declared above
+            return ans;
+
+
+    }
+
+
+    public static void main (String[] args){
+
+        int x = 3;
+        int y = 6;
+        int j = mySimpleSummingMethod(x,y);
+        System.out.println("The sum of x and y is : " + j);
+    }
+
+
+
+
+}
